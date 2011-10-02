184b23f61b71ce6da46bb912a83410dce0b27876
Omer Jakobinsky <omer@jakobinsky.com>
2011-10-02 17:59:59 +0100

    Added an option to install svm_light as a library

--- Library/Formula/svm_light.rb
+++ Library/Formula/svm_light.rb
@@ -6,8 +6,20 @@ class SvmLight < Formula
   homepage 'http://svmlight.joachims.org/'
   md5 '59768adde96737b1ecef123bc3a612ce'
 
+  def options
+    [
+      ['--libsvmlight_hideo', "builds shared object library that can be linked into other code using HIDEO"]
+    ]
+  end
+
   def install
-    system "make"
+    system "make all"
     bin.install ['svm_learn', 'svm_classify']
+
+    if ARGV.include? "--libsvmlight_hideo"
+      system "make libsvmlight_hideo"
+      (include+'svm_light').install Dir['*.h', '*.o']
+      lib.install Dir['*.so']
+    end
   end
 end
